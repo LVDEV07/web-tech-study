2d1fc296c554afe908149b2327ca9280deffb51e
Lucas <lucas.fst.dev@gmail.com>
2026-06-03 11:16:10 -0300

    feat: finalizando o layout de css e adicionando javascript no projeto

---
+++ form-web/js/script.js
@@ -0,0 +1,22 @@
+/*
+    Criando variáveis
+*/
+
+const nome = document.querySelector("#nome");
+const button = document.querySelector("button");
+const lista = document.querySelector(".lista");
+
+/*
+    Criando função
+*/
+
+button.addEventListener("click", (event) =>{
+    event.preventDefault();
+    console.log(nome.value)
+
+    // Incluir itens no html
+
+})
+
+
+
