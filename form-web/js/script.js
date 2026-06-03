bba542142e913b8a9113cdb9ffc5e18ef1d0640e
Lucas <lucas.fst.dev@gmail.com>
2026-06-03 13:29:20 -0300

    feat: adicionando campos de email e telefone

--- form-web/js/script.js
+++ form-web/js/script.js
@@ -3,6 +3,8 @@
 */
 
 const nome = document.querySelector("#nome");
+const email = document.querySelector("#email");
+const telefone = document.querySelector("#tel");
 const button = document.querySelector("button");
 const lista = document.querySelector(".lista");
 
@@ -13,13 +15,17 @@ const lista = document.querySelector(".lista");
 button.addEventListener("click", (event) =>{
     event.preventDefault();
     const inputValue = nome.value;
-    const templateHTML = `<li> ${inputValue} </li>`;
+    const inputValueEmail = email.value;
+    const inputValueTelefone = telefone.value;
+    const templateHTML = `<li> <hr> Nome: ${inputValue} <br> Email: ${inputValueEmail} <br> Telefone: ${inputValueTelefone} <br> </li>`;
     
     
     // Incluir itens no html
     lista.innerHTML += templateHTML
 
     nome.value = "" ;
+    email.value = "";
+    telefone.value = ""
 })
 
 
