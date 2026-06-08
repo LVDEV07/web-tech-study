b3a1620ba881445a4004feb8fd65d2248864d0f4
Lucas <lucas.fst.dev@gmail.com>
2026-06-08 11:31:56 -0300

    Update segundo exercicio

--- exemplo-js/ex02.js
+++ exemplo-js/ex02.js
@@ -1,3 +1,9 @@
-const javaScript = "Java Script"
+/*const javaScript = "Java Script"
 const js = javaScript[0] + javaScript[5]
-console.log(js)
+console.log(js)*/
+
+const mozzilla = "Mozzila Developer Network"
+
+const mdn = mozzilla[0] + mozzilla[8] + mozzilla[18]
+
+console.log(mdn)
