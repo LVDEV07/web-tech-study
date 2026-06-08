100ee682911f5e068ab3c1dd165920038978b983
Lucas <lucas.fst.dev@gmail.com>
2026-06-08 11:49:30 -0300

    Funções de calculo

--- exemplo-js/CalcNew.js
+++ exemplo-js/CalcNew.js
@@ -1,5 +1,31 @@
-function meuNome(nome) {
+/*function meuNome(nome) {
     console.log(nome)
 }
 
-meuNome("Joao")
+meuNome("Joao") */
+
+function soma(num1,num2){
+    console.log(num1+num2)
+}
+
+soma(10,10)
+soma(5,5)
+
+function subtracao(num1,num2){
+    console.log(num1-num2)
+}
+
+subtracao(10,10)
+
+function multiplicacao(num1,num2){
+    console.log(num1*num2)
+}
+
+multiplicacao(10,10)
+
+function divisao(num1,num2){
+    console.log(num1/num2)
+}
+
+divisao(10,10)
+
