6216f1b1a5c70111d17f3f760223efbe96b83542
Lucas <lucas.fst.dev@gmail.com>
2026-06-08 10:35:59 -0300

    feat: primeiras funções

---
+++ exemplo-js/texto.js
@@ -0,0 +1,11 @@
+let txt = "Estão chegando as provas "
+//tamanho da string
+console.log(txt.length);
+console.log(txt.replace("provas", "avaliações"))
+console.log(txt.toUpperCase())
+console.log(txt.toLowerCase())
+
+//Retorna a primeira posição da stream
+
+console.log(txt.indexOf("as"))
+console.log(txt.lastIndexOf("as"))
