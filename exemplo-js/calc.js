3b73af25f98d68a2edd5a036b28a97a2fbebcf96
Lucas <lucas.fst.dev@gmail.com>
2026-06-08 11:01:08 -0300

    feat: funções de cálculo

--- exemplo-js/calc.js
+++ exemplo-js/calc.js
@@ -1,2 +1,18 @@
 let num1 = 10
 let num2 = 10
+
+let soma = num1 + num2
+
+console.log(soma)
+
+let subtracao = num1 - num2
+
+console.log(subtracao)
+
+let multiplicacao = num1 * num2
+
+console.log(multiplicacao)
+
+let divisao = num1/num2
+
+console.log(divisao)
