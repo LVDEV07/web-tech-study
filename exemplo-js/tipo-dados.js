7f42be6a8638152ef2df51ade907dbe0791cb9e4
Lucas <lucas.fst.dev@gmail.com>
2026-06-08 10:48:13 -0300

    feat: tipos de dados

---
+++ exemplo-js/tipo-dados.js
@@ -0,0 +1,13 @@
+let nome = "Lucas Ferreira"
+
+let idade = 19
+
+let casado = false
+
+let valorProduto = 5.35
+
+console.log("Meu nome é: " + nome)
+console.log("Minha idade é: " + idade)
+console.log("Casado ? " + casado)
+console.log(`R$${valorProduto}`)
+console.log(typeof nome)
