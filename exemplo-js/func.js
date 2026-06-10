728815aef17180eb6e739238d303fbb776f2f974
Lucas <lucas.fst.dev@gmail.com>
2026-06-10 08:48:13 -0300

    feat: adicionando funções no HTML

---
+++ exemplo-js/func.js
@@ -0,0 +1,20 @@
+function minhaFuncao(){
+    document.write("Minha função")
+}
+function pularLinha(){
+
+    document.write("<br><br><br><br><br><br><br>")
+}
+
+function mostrarNome(nome){
+    pularLinha()
+    document.write(nome)
+    pularLinha()
+}
+
+
+minhaFuncao()
+pularLinha()
+minhaFuncao()
+mostrarNome("Lucas")
+mostrarNome("João")
