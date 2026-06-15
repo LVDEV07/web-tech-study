51167e849784df45e1d6e3d8ece0f8e2ea283ef4
Lucas <lucas.fst.dev@gmail.com>
2026-06-15 09:14:53 -0300

    Atualização das notas

--- nota-aluno/js/nota.js
+++ nota-aluno/js/nota.js
@@ -2,6 +2,7 @@
 const inputNota = document.getElementById("nota");
 const resultado = document.getElementById("resultado");
 const message = document.getElementById("message")
+const nome = document.getElementById("nome")
 
 
 function resultadoNotaClick(event) {
@@ -17,10 +18,10 @@ function resultadoNotaClick(event) {
 
 
     if (nota >= 6) {
-        resultado.innerHTML = "Aprovado";
+        resultado.innerHTML = `${nome.value} você está reprovado`;
     }
     else {
-        resultado.innerHTML = "Reprovado"
+        resultado.innerHTML = `${nome.value} você está reprovado`
     }
 
     inputNota.value = ""
