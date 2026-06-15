e2739c38646af1a80e86e8cc3eaaebee7f090ce5
Lucas <lucas.fst.dev@gmail.com>
2026-06-15 10:43:29 -0300

    feat: finalizando modal em js

--- pop-up/js/script.js
+++ pop-up/js/script.js
@@ -1,1 +1,19 @@
 //alert("Pop up")
+const button = document.querySelector("button")
+const popup = document.querySelector(".popup-wrapper")
+
+button.addEventListener("click", function(event){
+    popup.classList.add("d-block")
+})
+
+popup.addEventListener("click", function(event){
+    const classOfClickElement = event.target.classList[0]
+
+    const classNameListArray = ["popup-close", "popup-link", "popup-wrapper"]
+
+    
+
+    if(classNameListArray.includes(classOfClickElement)){
+        popup.classList.remove("d-block")
+    }
+})
